d5d2b91e4c4916489d9480c02b82a4c1709449ef
Denis Abramov <denis.m.abramov@mail.ru>
2020-12-04 16:15:36 +0300

    Finished project

--- HomeWork2-2/ViewController.swift
+++ HomeWork2-2/ViewController.swift
@@ -20,17 +20,31 @@ class ViewController: UIViewController {
     @IBOutlet var sliderGreenValue: UISlider!
     @IBOutlet var sliderBlueValue: UISlider!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
+       
+        // MARK: Screen
         screenColor.layer.cornerRadius = 10
-        
-        
-        
+
+        // MARK: Slider
+        sliderRedValue.minimumTrackTintColor = .red
+        sliderGreenValue.minimumTrackTintColor = .green
+        sliderBlueValue.minimumTrackTintColor = .blue
+
     }
 
-
+    @IBAction func sliderRedAction() {
+        labelRedValue.text = String(Int(sliderRedValue.value))
+        screenColor.backgroundColor = UIColor(red: CGFloat(sliderRedValue.value/255), green: CGFloat(sliderGreenValue.value/255), blue: CGFloat(sliderBlueValue.value/255), alpha: 1)
+    }
+    
+    @IBAction func sliderGreenAction() {
+        labelGreenValue.text = String(Int(sliderGreenValue.value))
+        screenColor.backgroundColor = UIColor(red: CGFloat(sliderRedValue.value/255), green: CGFloat(sliderGreenValue.value/255), blue: CGFloat(sliderBlueValue.value/255), alpha: 1)
+    }
+    @IBAction func sliderBlueAction() {
+        labelBlueValue.text = String(Int(sliderBlueValue.value))
+        screenColor.backgroundColor = UIColor(red: CGFloat(sliderRedValue.value/255), green: CGFloat(sliderGreenValue.value/255), blue: CGFloat(sliderBlueValue.value/255), alpha: 1)
+    }
 }
 
