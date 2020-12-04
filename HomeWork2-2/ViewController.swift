f1fe3f8ea8a2494c3e4126796ae6f2aad0c868f3
Denis Abramov <denis.m.abramov@mail.ru>
2020-12-04 11:59:25 +0300

    Added new elements

--- HomeWork2-2/ViewController.swift
+++ HomeWork2-2/ViewController.swift
@@ -8,10 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var screenColor: UIView!
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    @IBOutlet var labelRedValue: UILabel!
+    @IBOutlet var labelGreenValue: UILabel!
+    @IBOutlet var labelBlueValue: UILabel!
+    @IBOutlet var sliderRedValue: UISlider!
+    @IBOutlet var sliderGreenValue: UISlider!
+    @IBOutlet var sliderBlueValue: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        screenColor.layer.cornerRadius = 10
+        
+        
+        
     }
 
 
